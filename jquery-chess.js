71cf55313f03590382ee2c78139eee04ecabd2fc
Joshua David <Joshua@Joshua-David.net>
2013-02-22 21:02:27 -0800

    Plugin initializes pieces

--- jquery-chess.js
+++ jquery-chess.js
@@ -3,24 +3,21 @@
      * Extends JQuery to include some methods for chess games.
      */
     $.fn.extend({
-        chessBoard: function(options) {
+        chessboard: function(options) {
             var defaults = {
-                /**
-                 * Whether or not to show the header.
-                 *
-                 * The header contains information such as whose turn it is,
-                 * who is playing, whether a player is in check, and so on.
-                 */
                 showHeader: true,
                 showFooter: true,
                 squareSize: '40px',
                 showCaptured: true,
                 clearInside: true,
+		blackSquareColor: '#ddd',
+		whiteSquareColor: '#eee',
                 onPieceSelect: function() {},
                 onPieceMoveStart: function() {},
                 onPieceMoveEnd: function () {}
             };
             var options = $.extend(defaults, options);
+	    var pieces = [];
 
             var pieceHTML = {
                 'WK': '&#9812',
@@ -38,18 +35,99 @@
             };
             var rows = ['1', '2', '3', '4', '5', '6', '7', '8'];
             var cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+	    var generateBoardHTML = function() {
+		/**
+		 * Internal method to generate the HTML of the board
+		 */
+		var $board = $('<div/>');
+		for(var y = 0; y < rows.length; y++) {
+		    var $row = $('<div/>');
+		    for(var x = 0; x < cols.length; x++) {
+			$square = $('<div/>');
+			$square.addClass(cols[x] + rows[y]);
+			$square.css({
+			    'height': options.squareSize,
+			    'width': options.squareSize,
+			    'display': 'inline-block',
+			    'position': 'relative'
+			});
+			$row.append($square);
+		    }
+		    $board.append($row);
+		}
+		$board.find('div:even div:even').css({
+		    'background': options.whiteSquareColor
+		});
+		$board.find('div:odd div:even').css({
+		    'background': options.blackSquareColor
+		});
+		$board.find('div:even div:odd').css({
+		    'background': options.blackSquareColor
+		});
+		$board.find('div:odd div:odd').css({
+		    'background': options.whiteSquareColor
+		});
+		return $board;
+	    }
+
+	    var initializePieces = function($board) {
+		/**
+		 * Puts the pieces in their default locations.
+		 */
+		var colorNames = {'B': 'black', 'W': 'white'};
+		var pieceNames = {
+		    'R': 'rook',
+		    'N': 'knight',
+		    'B': 'bishop',
+		    'Q': 'queen',
+		    'K': 'king',
+		    'P': 'pawn'
+		}
+		var pieces = {
+		    'a1': 'BR', 'b1': 'BN', 'c1': 'BB', 'd1': 'BQ',
+		    'e1': 'BK', 'f1': 'BB', 'g1': 'BN', 'h1': 'BR',
+		    'a2': 'BP', 'b2': 'BP', 'c2': 'BP', 'd2': 'BP',
+		    'e2': 'BP', 'f2': 'BP', 'g2': 'BP', 'h2': 'BP',
+		    'a7': 'WP', 'b7': 'WP', 'c7': 'WP', 'd7': 'WP',
+		    'e7': 'WP', 'f7': 'WP', 'g7': 'WP', 'h7': 'WP',
+		    'a8': 'WR', 'b8': 'WN', 'c8': 'WB', 'd8': 'WQ',
+		    'e8': 'WK', 'f8': 'WB', 'g8': 'WN', 'h8': 'WR',
+		};
+		for(var i in pieces) {
+		    $board.find('.' + i).append(
+			$('<div/>')
+			.addClass(colorNames[pieces[i][0]])
+			.addClass(pieceNames[pieces[i][1]])
+			.html(pieceHTML[pieces[i]])
+			.css({
+			    'font-size': options.squareSize,
+			    'width': '100%',
+			    'height': '100%',
+			    'text-align': 'center',
+			    'position': 'absolute'
+			})
+		    );
+		}
+		return $board;
+	    }
+
             return this.each(function() {
-                $(this).data(
+                $(this).data({
                     options: options,
                     pieces: pieces
-                );
-
+                });
                 if(options.showHeader) {
-                    $header = $('<div/>').addClass('chessBoard-header');
+                    var $header = $('<div/>').addClass('chessBoard-header');
                     $(this).append($header);
                 }
-                
-
+		var $board = generateBoardHTML();
+		$(this).append($board);
+		initializePieces($board);
+                if(options.showFooter) {
+                    var $footer = $('<div/>').addClass('chessBoard-header');
+                    $(this).append($footer);
+                }
             });
         }
     });
